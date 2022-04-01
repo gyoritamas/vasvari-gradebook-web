149aa17f66032e690793715768d6c8eb808e05e6
gyoritamas <gyoritamasnoe@gmail.com>
2022-04-01 09:21:34 +0200

    Change error view

--- src/main/java/org/vasvari/gradebookweb/web/controller/advice/LoginControllerAdvice.java
+++ src/main/java/org/vasvari/gradebookweb/web/controller/advice/LoginControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.vasvari.gradebookweb.business.util.Problem;
 
 @ControllerAdvice
 @Slf4j
@@ -16,8 +17,15 @@ public class LoginControllerAdvice {
     public String exception(final Throwable throwable, final Model model) {
         log.error("Exception during execution of SpringSecurity application", throwable);
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
-        model.addAttribute("errorMessage", errorMessage);
+
+        try {
+            String details = errorMessage.substring(errorMessage.indexOf("[") + 1, errorMessage.indexOf("]"));
+            Problem problem = Problem.fromJson(details);
+            model.addAttribute("problem", problem);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", errorMessage);
+        }
+
         return "error";
     }
-
 }
